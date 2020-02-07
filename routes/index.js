5f499b90d2590bdcd5b665238a568f2dcc2d79b1
Anton Engman <anton.engman@knowit.se>
2020-02-07 11:14:17 +0100

    Added class. Added module in json

--- routes/index.js
+++ routes/index.js
@@ -16,5 +16,9 @@ export default function routes(app, addon) {
         });
     });
 
+    app.get('/my-requests', addon.authenticate(), function(req, res){
+        res.render('my-requests');
+    });
+
     // Add additional route handlers here...
 }
